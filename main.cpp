dab7c004ad130d08155cc25b75e4375a8291313c
Agst2k7 <carinhaprabaixo@gmail.com>
2026-05-04 13:25:43 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+
 int sortIndex(int WORD_TABLE_SIZE) {
 
     random_device rd; //  gera uma semente verdadeiramente aleatória
@@ -11,7 +12,9 @@ int sortIndex(int WORD_TABLE_SIZE) {
     return dist(gen);
 }
 
+
 void draw(const char status[], const int &lifes, const int &word_size) {
+    system("clear");
     cout << "Lifes: " << lifes << endl;
     cout << "--> ";
     for (int i=0; i<word_size; i++) {
@@ -20,118 +23,32 @@ void draw(const char status[], const int &lifes, const int &word_size) {
     cout << endl;
 }
 
-int SearchChar(const char& c, const char word[], const int &word_size) {
-    for (int i=0; i < word_size; i++) {
-        for (int j=0; j < word_size; j++) {
-            
+bool HaveChar(const char& c, const char word[], char status[], const int &word_size) {
+    int i = 0;
+    bool find = false;
+    while(i < word_size) {
+        if(c == word[i]) {
+            status[i] = c;
+            find = true;
         }
+        i++;
     }
+    return find;
 }
-
-bool validateGuess(const char &letter){
-    if()
-
-}
-
 
 int main(int argc, char* argv[]) {
 
     string palavras[100] = {
-    "cachorro",
-    "golfinho",
-    "borboleta",
-    "tartaruga",
-    "elefante",
-    "jacare",
-    "papagaio",
-    "formiga",
-    "cavalo",
-    "macaco",
-    "baleia",
-    "pinguim",
-    "aranha",
-    "leao",
-    "zebra",
-    "martelo",
-    "tesoura",
-    "lápis",
-    "espelho",
-    "guarda-chuva",
-    "mochila",
-    "lanterna",
-    "bússola",
-    "relógio",
-    "binóculo",
-    "violão",
-    "tapete",
-    "almofada",
-    "chave",
-    "garrafa",
-    "floresta",
-    "cachoeira",
-    "montanha",
-    "vulcão",
-    "deserto",
-    "glaciar",
-    "pantanal",
-    "savana",
-    "mangue",
-    "penhasco",
-    "tornado",
-    "arco-iris",
-    "geada",
-    "tempestade",
-    "bruma",
-    "abacaxi",
-    "maracujá",
-    "jiló",
-    "canjica",
-    "coxinha",
-    "paçoca",
-    "brigadeiro",
-    "tapioca",
-    "acarajé",
-    "goiaba",
-    "quiabo",
-    "pitanga",
-    "jabuticaba",
-    "cupuaçu",
-    "caju",
-    "farmácia",
-    "biblioteca",
-    "mercado",
-    "aeroporto",
-    "estádio",
-    "museu",
-    "parque",
-    "palácio",
-    "porto",
-    "delegacia",
-    "cartório",
-    "cemitério",
-    "aquário",
-    "oficina",
-    "galeria",
-    "bombeiro",
-    "enfermeira",
-    "arquiteto",
-    "fotógrafo",
-    "piloto",
-    "padeiro",
-    "motorista",
-    "barbeiro",
-    "pescador",
-    "fazendeiro",
-    "jornalista",
-    "carpinteiro",
-    "eletricista",
-    "cientista",
-    "veterinário",
-    "bicicleta",
-    "submarino",
-    "parafuso",
-    "campainha",
-    "peruca"
+    "apple","house","table","chair","water","light","plant","stone","bread","music",
+    "river","mountain","cloud","forest","ocean","flower","window","door","floor","ceiling",
+    "school","teacher","student","pencil","paper","notebook","computer","keyboard","screen","mouse",
+    "phone","camera","picture","movie","actor","artist","writer","reader","driver","pilot",
+    "train","plane","boat","car","truck","bicycle","engine","wheel","road","bridge",
+    "city","village","country","world","earth","space","star","planet","sun","moon",
+    "day","night","morning","evening","hour","minute","second","clock","watch","time",
+    "family","mother","father","brother","sister","friend","child","baby","people","person",
+    "food","fruit","meat","sugar","salt","coffee","tea","milk","juice","drink",
+    "happy","sad","angry","tired","strong","weak","fast","slow","big","small"
 };
     int WORD_TABLE_SIZE = 100;
 
@@ -148,11 +65,26 @@ int main(int argc, char* argv[]) {
 
     int lifes = 5;
 
-    cout << "PALAVRA: " << palavras[word_index] << endl;
 
     draw(status, lifes, word_size);
 
-    cout << "Digite uma letra:" << endl;
+    while (true) {
+        cout << "Digite uma letra:" << endl;
+
+        char input;
+        input = getchar();
+        getchar();
+
+        if (!HaveChar(input, word, status, word_size)) {
+            lifes--;
+            cout << "Letra errada!" << endl;
+        } else {
+            cout << "Acertou a letra!" << endl;
+        }
+
+        draw(status, lifes, word_size);
+        cout << "PALAVRA: " << palavras[word_index] << endl;
+    }
 
 
 
